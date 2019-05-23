e35592169674e96f731d1870b54738a4ab115067
jeremy coley <jeremy.coley@tactfactory.com>
2019-05-23 12:55:12 +0200

    Started layouts

--- app/src/main/java/com/imie/a2dev/teamculte/quizzmaster/views/InitGameActivity.java
+++ app/src/main/java/com/imie/a2dev/teamculte/quizzmaster/views/InitGameActivity.java
@@ -1,5 +1,8 @@
 package com.imie.a2dev.teamculte.quizzmaster.views;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.imie.a2dev.teamculte.quizzmaster.R;
@@ -8,10 +11,54 @@ import com.imie.a2dev.teamculte.quizzmaster.R;
  * Activity managing the create player and question fragments.
  */
 public class InitGameActivity extends AppCompatActivity {
+    /**
+     * Stores the step tag used to get the intent values.
+     */
+    public static final String STEP = "Step";
+    
+    /**
+     * Stores the create player string used to define the current step.
+     */
+    public static final String CREATE_PLAYER_STEP = "CreatePlayer";
 
+    /**
+     * Stores the create player string used to define the current step.
+     */
+    public static final String CREATE_QUESTION_STEP = "CreateQuestion";
+
+    /**
+     * Stores the create player string used to define the current step.
+     */
+    public static final String LOAD_PLAYER_STEP = "LoadPlayer";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init_game);
+
+        Fragment fragment;
+        FragmentManager fragmentManager = this.getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        
+        switch (this.getIntent().getStringExtra(STEP)) {
+            case CREATE_PLAYER_STEP:
+                fragment = new CreatePlayerFragment();
+                
+                break;
+            case CREATE_QUESTION_STEP:
+                fragment = new CreateQuestionFragment();
+                
+                break;
+            case LOAD_PLAYER_STEP:
+                fragment = new LoadPlayerFragment();
+                
+                break;
+            default:
+                
+                return;
+        }
+        
+        fragmentTransaction.replace(R.id.frame_content, fragment);
+        fragmentTransaction.commit();
     }
 }
--- app/src/main/java/com/imie/a2dev/teamculte/quizzmaster/views/MainActivity.java
+++ app/src/main/java/com/imie/a2dev/teamculte/quizzmaster/views/MainActivity.java
@@ -1,17 +1,60 @@
 package com.imie.a2dev.teamculte.quizzmaster.views;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import com.imie.a2dev.teamculte.quizzmaster.R;
 
 /**
  * Main activity.
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_main);
+        this.init();
     }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        
+        switch (view.getId()) {
+            case R.id.btn_new_game:
+                intent = new Intent(this, InitGameActivity.class);
+                intent.putExtra(InitGameActivity.STEP, InitGameActivity.CREATE_PLAYER_STEP);
+                
+                break;
+            
+            case R.id.btn_load_player:
+                intent = new Intent(this, InitGameActivity.class);
+                intent.putExtra(InitGameActivity.STEP, InitGameActivity.LOAD_PLAYER_STEP);
+                
+                break;
+            
+            case R.id.btn_settings:
+                intent = new Intent(this, SettingsActivity.class);
+                
+                break;
+             
+            default:
+                return;
+        }
+        
+        this.startActivity(intent);
+    }
+
+    /**
+     * Initializes the view components.
+     */
+    private void init() {
+        this.findViewById(R.id.btn_new_game).setOnClickListener(this);
+        this.findViewById(R.id.btn_load_player).setOnClickListener(this);
+        this.findViewById(R.id.btn_settings).setOnClickListener(this);
+    }
+    
 }
